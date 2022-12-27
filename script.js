d96dcecaac9ed9861b7c50e5dcd242deeae9aaa1
Shreejal Maharjan <shreejal27@gmail.com>
2022-12-27 19:55:14 +0545

    added js

---
+++ script.js
@@ -0,0 +1,12 @@
+const spaces = document.querySelectorAll('space');
+
+
+spaces.addEventListener('space', event => {
+  event.console.log("clicked");
+
+});
+
+
+function click(){
+    console.log("clicked");
+}
