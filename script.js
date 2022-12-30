c99f1ef78c2abe6da5137832a29af6f50d5c018c
Shreejal Maharjan <shreejal27@gmail.com>
2022-12-30 22:11:26 +0545

    swapTurns function created

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+var value = 0;
+
 var elements= document.querySelectorAll(".element"); //class name will give you array like object
 
+
+
 elements.forEach(element =>{
-  element.addEventListener('click', handleclick);
+  element.addEventListener('click', handleclick, {once:true});
 });
 
 function handleclick(e){
   console.log("Clicked");
+
+  swapTurns();
+  //place mark
+  //check for win
+  //check for draw
+  //Switch turns
 }
+function swapTurns(){
+  value= 1 - value;
+  console.log(value);
+}
+
+
