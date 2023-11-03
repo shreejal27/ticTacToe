502bb2b77948370486147798579b9094e480eb32
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-03 22:40:55 +0545

    added a Draw function

--- script.js
+++ script.js
@@ -1,60 +1,101 @@
 var value = 0;
 
-var elements= document.querySelectorAll(".element"); //class name will give you array like object
+var elements = document.querySelectorAll(".element"); //class name will give you array like object
 
 
-
-elements.forEach(element =>{
-  element.addEventListener('click', handleclick, {once:true});
+elements.forEach(element => {
+  element.addEventListener('click', handleclick, { once: true });
 });
 
-const checkWin= ()=>{
-  let boxtext =  document.getElementsByClassName("element");
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("element");
   let wins = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ]
-  wins.forEach(e =>{
-      if((boxtext[e[0]].innerHTML === boxtext[e[1]].innerHTML) && (boxtext[e[2]].innerHTML === boxtext[e[1]].innerHTML) && (boxtext[e[0]].innerHTML !== "")){
-        // document.querySelector(".info").innerText = boxtext[e[0]].innerText + "Won";
-        alert("Player " + boxtext[e[0]].innerText + " Won");
-      }
+  wins.forEach(e => {
+    if ((boxtext[e[0]].innerHTML === boxtext[e[1]].innerHTML) && (boxtext[e[2]].innerHTML === boxtext[e[1]].innerHTML) && (boxtext[e[0]].innerHTML !== "")) {
+      // document.querySelector(".info").innerText = boxtext[e[0]].innerText + "Won";
+      alert("Player " + boxtext[e[0]].innerText + " Won");
+      console.log("wonnnnnnnnnnnnn");
+      gameOver();
+    }
   })
 }
 
-function handleclick(e){
+function handleclick(e) {
   const cell = e.target;
   console.log("Clicked");
   placemark(cell);
-  checkWin();
+  if (checkWin()) {
+    return;
+  }
+  if (checkDraw()) {
+   return;
+  }
   swapTurns();
 
   //check for win
   //check for draw
   //Switch turns
 }
-function swapTurns(){
-  value= 1 - value;
+
+function swapTurns() {
+  value = 1 - value;
   console.log(value);
 }
 
-function placemark(cell){
-  if(value == 0){
+function placemark(cell) {
+  if (value == 0) {
     // cell.style.backgroundColor = "pink";
-    cell.textContent= "X";
+    cell.textContent = "X";
     // cell.innerHTML = '<img src="image.jpg" alt="A description of the image">';
-    
-  }
-  else{
-    // cell.style.backgroundColor = "red";
-    cell.textContent= "O";
 
   }
+  else {
+    // cell.style.backgroundColor = "red";
+    cell.textContent = "O";
+  }
 }
- 
+
+function gameOver() {
+  var elements = document.querySelectorAll(".element");
+  elements.forEach(element => {
+    element.removeEventListener('click', handleclick);
+  });
+  //delay the reset board by 2 seconds
+  setTimeout(resetBoard, 1500);
+}
+
+function resetBoard() {
+  var elements = document.querySelectorAll(".element");
+  elements.forEach(element => {
+    element.textContent = "";
+  });
+  elements.forEach(element => {
+    element.addEventListener('click', handleclick, { once: true });
+  });
+}
+
+const checkDraw = () => {
+  let boxtext = document.getElementsByClassName("element");
+  let isDraw = true;
+
+  for (let i = 0; i < boxtext.length; i++) {
+    if (boxtext[i].innerHTML === "") {
+      isDraw = false;
+      break;
+    }
+  }
+
+  if (isDraw) {
+    alert("It's a draw!");
+    gameOver();
+  }
+};
